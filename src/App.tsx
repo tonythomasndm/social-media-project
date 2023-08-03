b3ffa6c5b72288644e407341d83beb74e64fbc84
Tony Thomas <108207894+tonythomasndm@users.noreply.github.com>
2023-08-03 21:05:11 +0530

    pages intial made

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Main } from "./pages/main";
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Main />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
